1e635c103068541c6445c29f22862c7ad705cbda
Zou Xiang <zouxiang0639@icloud.com>
2019-07-23 16:13:24 +0800

    update

--- app/Consts/Admin/Client/NavCategoryConst.php
+++ app/Consts/Admin/Client/NavCategoryConst.php
@@ -5,17 +5,14 @@ namespace App\Consts\Admin\Client;
 class NavCategoryConst
 {
     const HEADER = 1;
-    const a = 2;
 
     const HEADER_DESC = '头部';
-    const a_DESC = 'a';
 
     public static function desc()
     {
 
         return [
             self::HEADER => self::HEADER_DESC,
-            self::a => self::a_DESC,
         ];
     }
 
