3ef42f92d39806491042cf9700bba28d1964857e
Zou Xiang <zouxiang0639@icloud.com>
2019-07-24 13:18:14 +0800

    update

--- app/Http/Composers/ComposerServiceProvider.php
+++ app/Http/Composers/ComposerServiceProvider.php
@@ -15,7 +15,7 @@ class ComposerServiceProvider extends ServiceProvider
     public function boot()
     {
 
-       // View::composer('admin::users.form', 'App\Http\Composers\UserFormComposer');
+       // View::composer('admin::users.form', 'App\Http\Composers\Admin\HeaderComposer');
 
     }
 
