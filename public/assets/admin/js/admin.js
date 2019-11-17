b2d246fa981c03ea422e1e46fb71112029521f1c
zouxiang <xiang.zou@54traveler.com>
2019-11-17 13:54:48 +0800

    update

--- public/assets/admin/js/admin.js
+++ public/assets/admin/js/admin.js
@@ -1,3 +1,71 @@
+
+//只能输入两个小数
+function clearNoNum(obj){
+    obj.value = obj.value.replace(/[^\d.]/g,"");  //清除“数字”和“.”以外的字符
+    obj.value = obj.value.replace(/\.{2,}/g,"."); //只保留第一个. 清除多余的
+    obj.value = obj.value.replace(".","$#$").replace(/\./g,"").replace("$#$",".");
+    obj.value = obj.value.replace(/^(\-)*(\d+)\.(\d\d).*$/,'$1$2.$3');//只能输入两个小数
+    if(obj.value.indexOf(".")< 0 && obj.value !=""){//以上已经过滤，此处控制的是如果没有小数点，首位不能为类似于 01、02的金额
+        obj.value= parseFloat(obj.value);
+    }
+}
+
+//只能输入两个小数并且可以是负数
+function clearNoNegative(obj){
+    obj.value = obj.value.replace(/[^\d.-]/g,"");  //清除“数字”和“.”以外的字符
+    obj.value = obj.value.replace(/\.{2,}/g,"."); //只保留第一个. 清除多余的
+    obj.value = obj.value.replace(/\-{2,}/g,"-"); //只保留第一个. 清除多余的
+    obj.value = obj.value.replace(/\-\./g,"-"); //只保留第一个. 清除多余的
+    obj.value = obj.value.replace(/^\./g, "");//第一位不能为.
+    obj.value = obj.value.replace(".","$#$").replace(/\./g,"").replace("$#$",".");
+    obj.value = obj.value.replace(/^(\-)*(\d+)\.(\d\d).*$/,'$1$2.$3');//只能输入两个小数
+    if(obj.value != '-' && obj.value.indexOf(".")< 0 && obj.value !=""){//以上已经过滤，此处控制的是如果没有小数点，首位不能为类似于 01、02的金额
+        obj.value= parseFloat(obj.value);
+    }
+}
+
+//只能整数
+function isNumber(obj){
+    obj.value = obj.value.replace(/[^\d]/g,"");  //清除“数字”和“.”以外的字符
+}
+
+
+//还原千分位
+function rmoney(s) {
+    return parseFloat(s.replace(/[^\d\.-]/g, ""));
+}
+
+//格式化金额千分位
+function fmoney(s, n) {
+
+    n = n > 0 && n <= 20 ? n : 2;
+    s = parseFloat((s + "").replace(/[^\d\.-]/g, "")).toFixed(n) + "";
+    var l = s.split(".")[0].split("").reverse(),
+      r = s.split(".")[1];
+    t = "";
+    for(i = 0; i < l.length; i ++ )
+    {
+        t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "");
+    }
+    return t.split("").reverse().join("") + "." + r;
+}
+
+//只能数字、大小写字母、下划线、中线和点
+function isCharacter(obj){
+    obj.value = obj.value.replace(/[^a-z|A-Z|0-9|\-|_|\.]/g,"");
+}
+
+//判断是否为数字
+function isDigital(val){
+    var regPos = /^\d+(\.\d+)?$/; //非负浮点数
+    var regNeg = /^(-(([0-9]+\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\.[0-9]+)|([0-9]*[1-9][0-9]*)))$/; //负浮点数
+    if(regPos.test(val) || regNeg.test(val)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
 $(function(){
     var locked = true;
     var config = initialAjAx;
